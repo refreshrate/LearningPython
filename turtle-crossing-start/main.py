637e74dd9f233f3a536e3b4983c2da34fb59df43
refreshrate <60144458+refreshrate@users.noreply.github.com>
2024-09-23 22:23:52 -0400

    Update main.py

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -2,11 +2,14 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
+from scoreboard import Scoreboard
 
 # from scoreboard import Scoreboard
+scoreboard = Scoreboard()
 
 screen = Screen()
 screen.setup(width=600, height=600)
+
 screen.tracer(0)
 
 player = Player()
@@ -22,7 +25,7 @@ while game_is_on:
     screen.update()
     car_manager.create_car()
     car_manager.move()
-
+    Scoreboard()
 
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
@@ -32,5 +35,5 @@ while game_is_on:
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
-
+        scoreboard.update_scoreboard()
 screen.exitonclick()
