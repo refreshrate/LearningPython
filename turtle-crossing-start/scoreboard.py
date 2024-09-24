f6518570cac70fe4081850c101c75cab5a427021
refreshrate <60144458+refreshrate@users.noreply.github.com>
2024-09-23 22:24:06 -0400

    Update scoreboard.py

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -1,5 +1,18 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+
+        self.score = 0
+        self.color("black")
+        self.penup()
+        self.goto(-280, 280)
+        self.hideturtle()
+        self.write(f"Score= {self.score}")
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score= {self.score + 1}")
